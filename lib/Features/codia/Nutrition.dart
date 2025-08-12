5024b68bfb436c26c72a473b604b9672e2ac8edf
Fitly Developer <fitlydeveloper@gmail.com>
2025-08-12 13:34:03 +0300

    Backup: sync local project into BACKUP68

--- lib/Features/codia/Nutrition.dart
+++ lib/Features/codia/Nutrition.dart
@@ -721,8 +721,7 @@ class _NutritionPage extends State<NutritionPage>
         _dataLoaded = true;
         _ready = true;
         if (mounted) setState(() {});
-        print(
-            '🧪 hydrate done | source=manager-cache | nonZero=${vitaminCount + mineralCount + otherCount}');
+        print('🧪 hydrate done | source=manager-cache | nonZero=${vitaminCount + mineralCount + otherCount}');
         return;
       }
     }
